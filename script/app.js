46927ad3229b0bf01072c8c680b675ae4df0d1ed
sax320@sina.com <sax320@sina.com>
2016-03-10 16:12:37 +0800

    2016-3-10

--- script/app.js
+++ script/app.js
@@ -1,99 +1,57 @@
-var app = angular.module('myApp', ['ngCookies', 'ngRoute', 'appCtrl', 'adminCtrl']);
+var app = angular.module('myApp', ['ngCookies', 'ngRoute', 'app.ctrl', 'admin.ctrl']);
+
+app.run(['$rootScope', '$location', function ($rootScope, $location) {
+    $rootScope.$on('$routeChangeSuccess', function (newV) {
+        $rootScope.path = $location.path()
+    });
+}]);
 
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
-            controller: 'homeCtrl',
+            controller: 'ctrl.home',
             templateUrl: 'views/home.html',
         })
         .when('/music', {
-            controller: 'musicCtrl',
+            controller: 'ctrl.music',
             templateUrl: 'views/music.html'
         })
         .when('/music/:id', {
-            controller: 'musicDetailCtrl',
+            controller: 'ctrl.music.detail',
             templateUrl: 'views/music-detail.html'
         })
         .when('/movie', {
-            controller: 'movieCtrl',
+            controller: 'ctrl.movie',
             templateUrl: 'views/movie.html'
         })
         .when('/movie/:id', {
-            controller: 'movieDetailCtrl',
+            controller: 'ctrl.movie.detail',
             templateUrl: 'views/movie-detail.html'
         })
         .when('/blog', {
-            controller: 'blogCtrl',
+            controller: 'ctrl.blog',
             templateUrl: 'views/blog.html'
         })
         .when('/blog/:id', {
-            controller: 'blogDetailCtrl',
+            controller: 'ctrl.blog.detail',
             templateUrl: 'views/blog-detail.html'
         })
         .when('/admin/music', {
-            controller: 'musicAdminCtrl',
+            controller: 'ctrl.admin.music',
             templateUrl: 'views/admin/music.html'
         })
         .when('/admin/movie', {
-            controller: 'movieAdminCtrl',
+            controller: 'ctrl.admin.movie',
             templateUrl: 'views/admin/movie.html'
         })
         .when('/admin/blog', {
-            controller: 'blogAdminCtrl',
+            controller: 'ctrl.admin.blog',
             templateUrl: 'views/admin/blog.html'
         })
         .otherwise({
             redirectTo: '/'
         });
 });
-
-var host = "http://localhost/ansux/1/index.php/blogs/";
-var postConfig = {
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    },
-    transformRequest: function (data) {
-        return $.param(data);
-    }
-};
-
-app.controller('signupCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.user = {
-        uid: '',
-        upwd: '',
-        pwd: '',
-        email: ''
-    };
-    $scope.submit = function () {
-        $http.post(host + 'user/signup', {
-            user: $scope.user
-        }, postConfig).success(function (res) {
-            console.log(res);
-        });
-    }
-}]);
-
-app.controller('signinCtrl', ['$scope', '$rootScope', '$http', '$cookieStore', function ($scope, $rootScope, $http, $cookieStore) {
-    $rootScope.account = $cookieStore.get('user');
-    $scope.user = {
-        uid: '',
-        upwd: ''
-    };
-    $scope.submit = function () {
-        $http.post(host + 'user/signin', {
-            user: $scope.user
-        }, postConfig).success(function (res) {
-            res = $.parseJSON(res);
-            if (res.status) {
-                $('#signinModal').modal('hide');
-                $cookieStore.put('user', res.user);
-                $rootScope.account = $cookieStore.get('user');
-            } else {
-                console.log(res.msg);
-            }
-        });
-    }
-}]);
 
 app.controller('navCtrl', ['$scope', '$rootScope', '$cookieStore', function ($scope, $rootScope, $cookieStore) {
     $scope.signout = function () {
--- script/controller.js
+++ script/controller.js
@@ -1,117 +1,134 @@
-var ctrl = angular.module('appCtrl', []);
-
 var host = "http://localhost/ansux/1/index.php/blogs/";
 var postConfig = {
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    },
-    transformRequest: function (data) {
-        return $.param(data);
-    }
+   headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+   },
+   transformRequest: function (data) {
+      return $.param(data);
+   }
 };
 
-// 首页模块
-ctrl.controller('homeCtrl', function ($scope, $http) {
-    $scope.title = 'i am home page';
-});
+angular.module('app.ctrl', [])
+   .controller('ctrl.signup', ['$scope', '$http', function ($scope, $http) {
+      $scope.user = {
+         uid: '',
+         upwd: '',
+         pwd: '',
+         email: ''
+      };
+      $scope.submit = function () {
+         $http.post(host + 'user/signup', {
+            user: $scope.user
+         }, postConfig).success(function (res) {
+            console.log(res);
+         });
+      }
+   }])
+   .controller('ctrl.signin', ['$scope', '$rootScope', '$http', '$cookieStore', function ($scope, $rootScope, $http, $cookieStore) {
+      $rootScope.account = $cookieStore.get('user');
+      $scope.user = {
+         uid: '',
+         upwd: ''
+      };
+      $scope.submit = function () {
+         $http.post(host + 'user/signin', {
+            user: $scope.user
+         }, postConfig).success(function (res) {
+            res = $.parseJSON(res);
+            if (res.status) {
+               $('#signinModal').modal('hide');
+               $cookieStore.put('user', res.user);
+               $rootScope.account = $cookieStore.get('user');
+            } else {
+               console.log(res.msg);
+            }
+         });
+      }
+   }])
 
-// 音乐模块
-ctrl.controller('musicCtrl', function ($scope, $http) {
-    $scope.title = 'i am other page';
-});
+   .controller('ctrl.home', function ($scope, $http) {
+      $scope.title = 'i am home page';
+   })
 
-// 音乐详情
-ctrl.controller('musicDetailCtrl', function ($scope, $routeParams, $location) {
-    var id = $routeParams.id;
-    if (id == 0) {
-        $location.path('/other');
-    }
-    $scope.title = 'i am otherDetailCtrl page';
-});
+   .controller('ctrl.music', function ($scope, $http) {
+      $scope.title = 'i am other page';
+   })
+   .controller('ctrl.music.detail', function ($scope, $routeParams, $location) {
+      var id = $routeParams.id;
+      if (id == 0) {
+         $location.path('/other');
+      }
+      $scope.title = 'i am otherDetailCtrl page';
+   })
 
-// 电影模块
-ctrl.controller('movieCtrl', function ($scope) {
-    $scope.title = 'i am other page';
-});
+   .controller('ctrl.movie', function ($scope) {
+      $scope.title = 'i am other page';
+   })
+   .controller('ctrl.movie.detail', function ($scope, $routeParams, $location) {
+      var id = $routeParams.id;
+      if (id == 0) {
+         $location.path('/other');
+      }
+      $scope.title = 'i am otherDetailCtrl page';
+   })
 
-// 电影详情
-ctrl.controller('movieDetailCtrl', function ($scope, $routeParams, $location) {
-    var id = $routeParams.id;
-    if (id == 0) {
-        $location.path('/other');
-    }
-    $scope.title = 'i am otherDetailCtrl page';
-});
+   .controller('ctrl.blog', function ($scope, $http, $timeout, $rootScope) {
+      $http.get(host + 'blog/getlist').success(function (res) {
+         $scope.blogList = $.parseJSON(res);
+      });
 
-// 博客模块
-ctrl.controller('blogCtrl', function ($scope, $http, $timeout, $rootScope) {
-    $scope.list = function () {
-        $http.post(host + 'blog/getlist', {}).success(function (res) {
-            $scope.blogList = $.parseJSON(res);
-        });
-    }
+      $scope.title = 'i am other page';
+      $scope.category = {
+         name: ''
+      }
 
-    $scope.title = 'i am other page';
-    $scope.category = {
-        name: ''
-    }
-
-    $scope.existCateFlag = false;
-    $scope.existCateName = function () {
-        if ($scope.category.name != undefined && $scope.category.name.length != 0) {
+      $scope.existCateFlag = false;
+      $scope.existCateName = function () {
+         if ($scope.category.name != undefined && $scope.category.name.length != 0) {
             $http.post(host + 'category/existName', {
-                name: $scope.category.name
+               name: $scope.category.name
             }, postConfig).success(function (res) {
-                res = $.parseJSON(res);
-                $scope.existCateFlag = res ? true : false;
+               res = $.parseJSON(res);
+               $scope.existCateFlag = res ? true : false;
             });
-        }
-    }
+         }
+      }
 
-    $scope.submit = function () {
-        $http.post(host + 'category/create', {
+      $scope.submit = function () {
+         $http.post(host + 'category/create', {
             category: $scope.category
-        }, postConfig).success(function (res) {
+         }, postConfig).success(function (res) {
             $('#cateModal').modal('hide');
             $timeout(function () {
-                $scope.category.name = '';
-                $scope.category.name.reset();
+               $scope.category.name = '';
             }, 1000)
-        });
-    }
+         });
+      }
 
-    $scope.showBlogModal = function () {
-        $http.post(host + 'category/getList', {}).success(function (res) {
+      $scope.showBlogModal = function () {
+         $http.get(host + 'category/getList').success(function (res) {
             $scope.cateList = $.parseJSON(res);
-            if ($scope.cateList != null) {
-                $scope.cateList[0].isSelected = true;
-            }
-        });
-    }
+         });
+      }
 
-    $scope.blog = {
-        author: $rootScope.account.id,
-        category: '1'
-    };
-    $scope.blogSubmit = function () {
-        $http.post(host + 'blog/create', {
+      $scope.blogSubmit = function () {
+         $scope.blog = {
+            author: $rootScope.account.id,
+            category: '1'
+         };
+         $http.post(host + 'blog/create', {
             blog: $scope.blog
-        }, postConfig).success(function (res) {
+         }, postConfig).success(function (res) {
             $('#blogModal').modal('hide');
-        });
-    }
+         });
+      }
+   })
+   .controller('ctrl.blog.detail', function ($scope, $routeParams, $location) {
+      var id = $routeParams.id;
+      if (id == 0) {
+         $location.path('/other');
+      }
+      $scope.title = 'i am otherDetailCtrl page';
+   });
 
-});
-
-
-// 博客详情
-ctrl.controller('blogDetailCtrl', function ($scope, $routeParams, $location) {
-    var id = $routeParams.id;
-    if (id == 0) {
-        $location.path('/other');
-    }
-    $scope.title = 'i am otherDetailCtrl page';
-});
-
-
-var admin = angular.module('adminCtrl', []);
+angular.module('admin.ctrl', []);
